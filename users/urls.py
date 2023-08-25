0bd760c5d8bd6c98adb3663b639e371ca28d8bd6
taeho0888 <taeho0888@naver.com>
2023-08-26 01:53:51 +0900

    회원가입 로직 수정

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('login/refresh/', TokenRefreshView.as_view()),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('list/', UserView.as_view(), name="user"),
+    path('', UserView.as_view(), name="user"),
     path('verify-email/', VerifyEmailView.as_view(), name='verify-email'),
 
 
