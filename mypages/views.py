f9bb5e3130446a9e99902fee812c2d59f48fe0fb
leejiwon <2jiwone@ewhain.net>
2023-08-25 23:37:07 +0900

    feat:mypage

--- mypages/views.py
+++ mypages/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from users.models import *
+from users.serializers import *
+from rest_framework import views
+from rest_framework.response import Response
+from studies.models import Study
+from studies.serializers import StudySerializer
 # Create your views here.
+
+class MyPageView(views.APIView):
+
+    #게시한 스터디 
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        studies = Study.objects.filter(writer=user)
+        serializer = StudySerializer(studies, many=True)
+        return Response(serializer.data)
