1e3c0101dbb79048c3619a702bd708b3f09d6b88
soyeoneeii <syp0463@gmail.com>
2023-08-25 21:50:00 +0900

    Chore: url  수정

--- studies/urls.py
+++ studies/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .import views
 
 router = DefaultRouter()
-# 모든 StudyViewSet 관련 URL은 /studys/로 시작함
-router.register(r'studys', views.StudyViewSet)
+# 모든 StudyViewSet 관련 URL은 /study/로 시작함
+router.register(r'study', views.StudyViewSet)
 
 urlpatterns = [
     path('api/',include(router.urls))
